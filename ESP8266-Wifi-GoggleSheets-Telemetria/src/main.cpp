1d4de906b5e5513f247932c642fe5c1eff3bd7d3
Ricardo Felippe <rica_felippe@yahoo.com.br>
2024-09-04 16:12:28 -0300

    Split "cpp"

--- ESP8266-Wifi-GoggleSheets-Telemetria/src/main.cpp
+++ ESP8266-Wifi-GoggleSheets-Telemetria/src/main.cpp
@@ -11,21 +11,6 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-//WiFi----------------------
-//const char* ssid = "NET_5G";
-//const char* password = "4F6FAED8";
-
-// WI-FI-UTFPR_ALUNO
-//const char* ssid = "a2558467";
-//const char* password = "xyvamy95";
-
-
-const char* ssid = "@ksa5to2";
-const char* password = "@liv#162125";
-
-//Config WI-FI-----------------------------------------------------------
-//const char* ssid        = "insira-aqui-o-nome-da-sua-rede-wifi";
-//const char* password    = "insira-aqui-a-senha-da-sua-rede-wifi";
  
 // Este dado retira-se do SCRIPT na planilha GOOGLE.
 const char* GScriptId   = "AKfycbyF6Bq7r180NjNaCKJWZIh05YI5v9mlzbBgvVm8NX6FTDq7GXOc0b3ceJZlyWXt-c90";
---
+++ ESP8266-Wifi-GoggleSheets-Telemetria/src/wifi.cpp
@@ -0,0 +1,17 @@
+//WiFi----------------------
+//const char* ssid = "NET_5G";
+//const char* password = "4F6FAED8";
+
+// WI-FI-UTFPR_ALUNO
+//const char* ssid = "a2558467";
+//const char* password = "xyvamy95";
+
+void on_wifi(){
+
+const char* ssid = "@ksa5to2";
+const char* password = "@liv#162125";
+}
+
+//Config WI-FI-----------------------------------------------------------
+//const char* ssid        = "insira-aqui-o-nome-da-sua-rede-wifi";
+//const char* password    = "insira-aqui-a-senha-da-sua-rede-wifi";
